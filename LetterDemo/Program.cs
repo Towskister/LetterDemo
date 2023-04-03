9b11e8728b54884c0678aabd559eec5b064c06b9
Towskister <Towskister@gmail.com>
2023-04-03 19:50:29 -0400

    Update Program.cs

--- LetterDemo/Program.cs
+++ LetterDemo/Program.cs
@@ -8,20 +8,31 @@ class LetterDemo
 }
 
 // Write your classes here
-    class public LetterDemo
-{
-    
-}
+    class public Letter
+    {
+        public string recipientName { get; set;}
+        public string mailedDate { get; set;}
+    }
 
 /*
  * Instructions
 
-Create an application class named LetterDemo that instantiates objects of two classes named Letter and CertifiedLetter and that demonstrates all their methods.
+Create an application class named LetterDemo that instantiates
+ objects of two classes named Letter and CertifiedLetter and 
+that demonstrates all their methods.
 
-The classes are used by a company to keep track of letters they mail to clients. The Letter class includes auto-implemented properties for the Name of the recipient and the Date mailed (stored as strings).
+The classes are used by a company to keep track of letters 
+they mail to clients. The Letter class includes 
+auto-implemented properties for the Name of the recipient 
+and the Date mailed (stored as strings).
 Step 2
 
-Next, include a ToString() method that overrides the Object class’s ToString() method and returns a string that contains the name of the class (using GetType()) and the Letter’s data field values.
+Next, include a ToString() method that overrides the Object 
+class’s ToString() method and returns a string that contains 
+the name of the class (using GetType()) and the Letter’s 
+data field values.
 
-Create a child class named CertifiedLetter that includes an auto-implemented property TrackingNumber (of type string) that holds a tracking number for the letter.
+Create a child class named CertifiedLetter that includes 
+an auto-implemented property TrackingNumber (of type string) 
+that holds a tracking number for the letter.
 */
