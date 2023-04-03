535445369139dc24bf4da2370ddf086c4527b297
Towskister <Towskister@gmail.com>
2023-04-03 15:16:50 -0400

    Add project files.

---
+++ LetterDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+class LetterDemo
+{
+    static void Main()
+    {
+        // Write your code here
+    }
+}
+
+// Write your classes here
+
+
+/*
+ * Instructions
+
+Create an application class named LetterDemo that instantiates objects of two classes named Letter and CertifiedLetter and that demonstrates all their methods.
+
+The classes are used by a company to keep track of letters they mail to clients. The Letter class includes auto-implemented properties for the Name of the recipient and the Date mailed (stored as strings).
+Step 2
+
+Next, include a ToString() method that overrides the Object class’s ToString() method and returns a string that contains the name of the class (using GetType()) and the Letter’s data field values.
+
+Create a child class named CertifiedLetter that includes an auto-implemented property TrackingNumber (of type string) that holds a tracking number for the letter.
+*/
